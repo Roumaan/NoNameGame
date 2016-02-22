19812584e4e647c1e083dd1e2c641e4ccb66b880
roumaan <romanchik_2002@mail.ru>
2016-02-22 20:51:29 +0300

    Development of EgyptBackground class started

---
+++ app/src/main/java/ru/roumaan/nonamegame/EgyptBackground.java
@@ -0,0 +1,10 @@
+package ru.roumaan.nonamegame;
+
+
+import android.content.Context;
+
+public class EgyptBackground extends Background {
+    public EgyptBackground(Context context, int width, int height) {
+        
+    }
+}
