601edd4d738d00e8a337f4759fbb7c7a355e3dd7
Roumaan <romanchik_2002@mail.ru>
2016-03-01 22:07:44 +0300

    EgyptCampaignPlayer and blanks for other Egypt Campaign classes

---
+++ app/src/main/java/ru/roumaan/nonamegame/EgyptBoard.java
@@ -0,0 +1,18 @@
+package ru.roumaan.nonamegame;
+
+
+import android.content.Context;
+
+public class EgyptBoard extends Board {
+    public EgyptBoard(Context context, int width, int height, int symbols, int speed) {
+
+    }
+
+    public void setGrade(int grade) {
+
+    }
+
+    public boolean isBeyond() {
+        return false;
+    }
+}
---
+++ app/src/main/java/ru/roumaan/nonamegame/EgyptButtons.java
@@ -0,0 +1,14 @@
+package ru.roumaan.nonamegame;
+
+
+import android.content.Context;
+
+public class EgyptButtons extends  Buttons {
+    public EgyptButtons(Context context, int width, int height) {
+
+    }
+
+    public void setGrade(int grade) {
+
+    }
+}
