bcda72d55029bb7739d1f2d76892ab92ef616e5d
Roumaan <romanchik_2002@mail.ru>
2016-03-07 16:29:07 +0300

    EgyptBackground

--- app/src/main/java/ru/roumaan/nonamegame/EgyptBackground.java
+++ app/src/main/java/ru/roumaan/nonamegame/EgyptBackground.java
@@ -2,13 +2,66 @@ package ru.roumaan.nonamegame;
 
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.util.Random;
 
 public class EgyptBackground extends Background {
+    Bitmap GoldGradeBitmap;
+    Bitmap SilverGradeBitmap;
+    Bitmap BronzeGradeBitmap;
+
     public EgyptBackground(Context context, int width, int height) {
-        
+        x = -10;
+        y = -10;
+
+        w = width + 20;
+        h = height + 20;
+
+        GoldGradeBitmap = BitmapFactory.decodeResource(
+                context.getResources(),
+                R.drawable.standart_background);
+        GoldGradeBitmap = Bitmap.createScaledBitmap(GoldGradeBitmap, w, h, false);
+
+        SilverGradeBitmap = BitmapFactory.decodeResource(
+                context.getResources(),
+                R.drawable.standart_background);
+        SilverGradeBitmap = Bitmap.createScaledBitmap(SilverGradeBitmap, w, h, false);
+
+        BronzeGradeBitmap = BitmapFactory.decodeResource(
+                context.getResources(),
+                R.drawable.standart_background);
+        BronzeGradeBitmap = Bitmap.createScaledBitmap(BronzeGradeBitmap, w, h, false);
     }
 
     public void setGrade(int grade) {
-        
+        switch (grade) {
+            case 3:
+                bitmap = GoldGradeBitmap;
+                break;
+            case 2:
+                bitmap = SilverGradeBitmap;
+                break;
+            case 1:
+                bitmap = BronzeGradeBitmap;
+                break;
+        }
+        sprite.setBitmap(bitmap);
+    }
+
+    @Override
+    public void update(int ms) {
+        super.update(ms);
+
+        Random random = new Random();
+
+        int i = random.nextInt(10);
+        x-=i;
+        sprite.setX(x);
+
+        int j = random.nextInt(10);
+        y-=j;
+        sprite.setY(y);
     }
 }
--- app/src/main/java/ru/roumaan/nonamegame/MainActivity.java
+++ app/src/main/java/ru/roumaan/nonamegame/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        StandardCampaignPlayer egypt = new StandardCampaignPlayer(this, 50000);
+        EgyptCampaignPlayer egypt = new EgyptCampaignPlayer(this, 50000, 30000, 12000, 10, 1.2, 10);
         setContentView(egypt);
     }
 }
