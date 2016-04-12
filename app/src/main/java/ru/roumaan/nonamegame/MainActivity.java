e012d934ac2598a0b2076c3af20defa15672055b
Roumaan <romanchik_2002@mail.ru>
2016-04-12 23:13:53 +0300

    Speed increase

--- app/src/main/java/ru/roumaan/nonamegame/MainActivity.java
+++ app/src/main/java/ru/roumaan/nonamegame/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EgyptCampaignPlayer egypt = new EgyptCampaignPlayer(this, 50000, 30000, 12000, 50, 1.2, 10);
+        EgyptCampaignPlayer egypt = new EgyptCampaignPlayer(this, 50000, 30000, 12000, 100, 1.2, 10);
         setContentView(egypt);
     }
 }
